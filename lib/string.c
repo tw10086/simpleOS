3f2ef1274b0a6e8ad9ca659824e4d0e38d73aa3e
wei <917009592@qq.com>
2017-06-26 10:31:08 +0800

    test

--- lib/string.c
+++ lib/string.c
@@ -3,6 +3,8 @@
 #include "debug.h"
 
 void memset(void* dst_,uint8_t value,uint32_t size){
-    ASSERT(dst_=NULL);
-    uint8_t* dst=
+	ASSERT(dst_!=NULL);
+	uint8_t* dst=(uint8_t*)dst;
+	while(size-->0)
+		*dst++=value;
 }
