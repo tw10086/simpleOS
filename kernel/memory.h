06156050969c2e9550e75098b945d7d4019f0874
wei <917009592@qq.com>
2017-07-07 11:15:38 +0800

    add thread

--- kernel/memory.h
+++ kernel/memory.h
@@ -2,6 +2,19 @@
 #define _KERNEL_MEMORY_H
 #include "bitmap.h"
 #include "stdint.h"
+
+enum pool_flags{
+	PF_KERNEL=1,
+	PF_USER=2
+};
+
+#define PG_P_1 1
+#define PG_P_0 0
+#define PG_RW_R 0
+#define PG_RW_W 2
+#define PG_US_S 0
+#define PG_US_U 4
+
 struct virtual_addr{
 	struct bitmap vaddr_bitmap;
 	uint32_t vaddr_start;
---
+++ thread/thread.h
@@ -0,0 +1,60 @@
+#ifndef _THREAD_THREAD_H
+#define _THREAD_THREAD_H
+#include "stdint.h"
+
+enum task_status{
+	TASK_RUNNING,
+	TASK_READY,
+	TASK_BLOCKED,
+	TASK_WAITING,
+	TASK_HANGING,
+	TASK_DIED
+};
+
+struct intr_stack{
+	uint32_t vec_no;
+	uint32_t edi;
+	uint32_t esi;
+	uint32_t ebp;
+	uint32_t esp_dummy;
+
+	uint32_t dbx;
+	uint32_t edx;
+	uint32_t eax;
+	uint32_t gs;
+	uint32_t fs;
+	uint32_t es;
+	uint32_t ds;
+	
+	uint32_t err_code;
+	void (*eip) (void);
+	uint32_t cs;
+	uint32_t eflags;
+	void* esp;
+	uint32_t ss;
+};
+
+struct thread_stack{
+	uint32_t ebp;
+	uint32_t ebx;
+	uint32_t edi;
+	uint32_t esi;
+	
+	void (*eip)(thread_func* func,void* func_arg);
+	void (*unused_retaddr);
+	thread_func* function;
+	void* func_arg;
+};
+
+struct task_struct{
+	uint32_t* self_kstack;
+	enum task_status statuc;
+	uint8_t priority;
+	char name[16];
+	uint32_t stack_magic;
+};
+
+void thread_create(struct task_struct* pthread,thread_func function,void* func_arg);
+void init_thread(struct task_struct* pthread,char* name,int prio);
+struct task_struct* thread_start(char* name,int prio,thread_func function,void* fun_arg);
+#endif
